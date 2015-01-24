79ba555699367ad5cb3af355f19f79baf0b133d3
Steve Canny <scanny@cisco.com>
2015-01-24 15:31:23 -0800

    test: establish test infrastructure

---
+++ tests/test_gitlib.py
@@ -0,0 +1,55 @@
+# encoding: utf-8
+
+"""
+Unit test suite for the githelpers module.
+"""
+
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
+
+from zipfile import ZipFile
+
+import py
+import pytest
+
+
+TEST_REPO_ZIP = str(py.path.local(__file__).dirpath('test-repo.zip'))
+
+
+# shared fixtures ----------------------------------------------------
+
+@pytest.fixture(scope='module')
+def module_test_repo(request):
+    """
+    Extract the test repo into a temporary directory having module scope.
+    """
+    test_repo_dir = py.test.ensuretemp('test-repo')
+    zip_file = ZipFile(TEST_REPO_ZIP)
+    zip_file.extractall(str(test_repo_dir))
+    return test_repo_dir
+
+
+@pytest.fixture
+def readonly_test_repo(request, module_test_repo):
+    """
+    Change the current working directory to the module scope test repo,
+    restoring the original working directory after the test.
+    """
+    cwd = module_test_repo.chdir()
+    request.addfinalizer(lambda: cwd.chdir())
+
+
+@pytest.fixture
+def new_test_repo(request, tmpdir):
+    """
+    Extract the test repo into a temporary directory, making that temp
+    directory the current working directory. Restore the original current
+    working directory after request.
+    """
+    test_repo_dir = tmpdir.mkdir("test-repo")
+    zip_file = ZipFile(TEST_REPO_ZIP)
+    zip_file.extractall(str(test_repo_dir))
+    cwd = test_repo_dir.chdir()
+    request.addfinalizer(lambda: cwd.chdir())
+    return test_repo_dir
