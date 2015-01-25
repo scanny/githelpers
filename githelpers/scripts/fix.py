d81ec5ce901c97e7ec98add6444bdfff5547197c
Steve Canny <scanny@cisco.com>
2015-01-24 16:26:40 -0800

    fix: add fix._fix()

--- githelpers/scripts/fix.py
+++ githelpers/scripts/fix.py
@@ -16,6 +16,30 @@ import sys
 from .exceptions import ExecutionError
 
 
+def _exit_if_not_valid_in_context():
+    """
+    Exit with an error message if the current working directory is not in a Git
+    repository or if the working directory is dirty. Otherwise, return None.
+    """
+    raise NotImplementedError
+
+
+def _exit_if_not_valid(commit_ref):
+    """
+    Exit with an error message if *commit_ref* does not identify a commit in
+    the repository. Otherwise, return None.
+    """
+    raise NotImplementedError
+
+
+def _checkout_branch_and_reset_to(branch_name, commit_ref):
+    """
+    Check out *branch_name* and reset it hard to *commit_ref*. Create the
+    branch at HEAD it doesn't exist.
+    """
+    raise NotImplementedError
+
+
 def _fix(commit_ref):
     """
     Move 'fixit' branch to the commit identified by *commit_ref*. Exits with
@@ -23,7 +47,9 @@ def _fix(commit_ref):
     repository or if *commit_ref* does not identify a commit in the
     repository.
     """
-    raise NotImplementedError
+    _exit_if_not_valid_in_context()
+    _exit_if_not_valid(commit_ref)
+    _checkout_branch_and_reset_to('fixit', commit_ref)
 
 
 def main(argv=None):
