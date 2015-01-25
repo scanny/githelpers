8f678a2da9f9bd93dc0ffb226f78ac1404ce53f8
Steve Canny <scanny@cisco.com>
2015-01-24 16:23:00 -0800

    fix: add fix.main()

--- githelpers/scripts/fix.py
+++ githelpers/scripts/fix.py
@@ -11,6 +11,29 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
+import sys
+
+from .exceptions import ExecutionError
+
+
+def _fix(commit_ref):
+    """
+    Move 'fixit' branch to the commit identified by *commit_ref*. Exits with
+    an error message if the current working directory is not in a Git
+    repository or if *commit_ref* does not identify a commit in the
+    repository.
+    """
+    raise NotImplementedError
+
 
 def main(argv=None):
-    raise NotImplementedError
+    """
+    Entry point for 'fix' script.
+    """
+    commit_ish = sys.argv[1] if argv is None else argv[1]
+    try:
+        _fix(commit_ish)
+    except ExecutionError as e:
+        print(e.message, file=sys.stderr)
+        return e.return_code
+    return 0
