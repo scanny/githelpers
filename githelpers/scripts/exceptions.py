56d7ed1ecbbde4f680c511c39c745742ab29b594
Steve Canny <scanny@cisco.com>
2015-01-27 22:38:26 -0800

    exc: add ExecutionError

---
+++ githelpers/scripts/exceptions.py
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+"""
+Exceptions for scripts.
+"""
+
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
+
+
+class ExecutionError(Exception):
+    """
+    Execption for returning non-zero error codes directly to main().
+    """
+    def __init__(self, message, return_code=1):
+        self._message = message
+        self._return_code = return_code
+
+    def __str__(self):
+        return self._message
+
+    @property
+    def message(self):
+        return self._message
+
+    @property
+    def return_code(self):
+        return self._return_code
